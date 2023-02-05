17dfe84561b7cbb711cedc3c1583410854aa70e5
Gideon Obiasor <gideonoiasor@gmail.com>
2023-02-05 20:38:01 +0100

    Task 0-7

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def hello_HBNB():
     """Displays 'Hello HBNB'"""
-    return "Hello HBNB"
+    return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
