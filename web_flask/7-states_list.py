bedb5fda4b6fb21fafeb2a40f5492cdc79b89e1e
Gideon Obiasor <gideonoiasor@gmail.com>
2023-02-06 13:30:36 +0100

    Task 8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
-"""Fetches data from storage engine and displays in li tag"""
+"""Fetches data from storage engine and displays in li tag
+Listens on 0.0.0.0:5000
+
+"""
 from models import storage
 from models.state import State
 from flask import Flask, render_template
 
-
 app = Flask(__name__)
 
 
-@app.route('/states_list', strict_slashes=False)
+@app.route("/states_list", strict_slashes=False)
 def states_list():
     """Displays html page of state in a list format"""
     states = storage.all(State)
-    return render_template('7-states_list.html', states=states)
+    return render_template("7-states_list.html", states=states)
 
 
 @app.teardown_appcontext
